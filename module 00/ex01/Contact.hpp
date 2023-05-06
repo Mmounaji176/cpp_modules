a45ee842918bda4afb96935ea6146ed81bdedb2c
mouad <mouad@gmail.com>
2023-05-06 16:21:33 +0100

    cpp00

---
+++ module 00/ex01/Contact.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmounaji <mmounaji@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 14:32:01 by mmounaji          #+#    #+#             */
+/*   Updated: 2023/05/06 15:24:26 by mmounaji         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+#include <string>
+
+class Contact
+{
+	private:
+		std::string 	first_name;
+		std::string 	last_name;
+		std::string 	nickname;
+		std::string 	phone_number;
+		std::string 	darkest_secret;
+		int				index;
+	public:
+	
+		
+};
